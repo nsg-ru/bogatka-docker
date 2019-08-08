ad4668482e17904a20585689dc50841df71c63cf
Igor Mosunov <imosunov@nsg.ru>
2019-08-08 14:01:06 +0300

    add bg_upgr script

--- manager01.sh
+++ manager01.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+. ./bogatka.lib
+
 ### init swarm
 #docker swarm init --advertise-addr 10.33.33.31
 
@@ -25,24 +28,4 @@ docker run -t \
   --cap-add=NET_ADMIN \
   ileamo/acari-server migrate
 
-# Server skolkovo
-# --name and --hostname must be the same
-docker run -t \
-  --name skolkovo \
-  --hostname skolkovo \
-  --network bogatka-net \
-  --restart unless-stopped \
-  -e DB_HOST=acari-server-db \
-  -e ZBX_API_URL="http://zabbix-web-nginx-pgsql" \
-  -e ZBX_WEB_PORT=10443 \
-  -e ZBX_SND_HOST="zabbix-server-pgsql" \
-  -p 443:50443 \
-  -p 51019:50019 \
-  -v /etc/ssl/acari:/etc/ssl/acari:ro \
-  -v /var/log/acari_foo:/var/log \
-  -v /etc/localtime:/etc/localtime:ro \
-  --link acari-server-db \
-  --link zabbix-web-nginx-pgsql \
-  --cap-add=NET_ADMIN \
-  --device /dev/net/tun:/dev/net/tun \
-  -d ileamo/acari-server
+run_bogatka skolkovo
